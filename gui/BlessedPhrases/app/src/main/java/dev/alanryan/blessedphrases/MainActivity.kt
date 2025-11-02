12400c867b435c17f8a881202bf1f2938440b9a0
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-02 20:48:19 -0300

    main: feat (BlessedPhrases) - Added IDE config and update app UI/resources
    - Added IDE project configuration and apply UI/resource updates to the BlessedPhrases Android module. This includes changes to the app's main activity, theme, and string/array resources to improve presentation and behavior

--- gui/BlessedPhrases/app/src/main/java/dev/alanryan/blessedphrases/MainActivity.kt
+++ gui/BlessedPhrases/app/src/main/java/dev/alanryan/blessedphrases/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.alanryan.blessedphrases.ui.theme.BlessedPhrasesTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +40,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BlessedPhrasesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainScreen(
+                        m = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +50,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(m: Modifier = Modifier) {
+    var resultado by remember { mutableStateOf("Deus falará com você aqui") }
+
+    val lista = stringArrayResource(R.array.phrases)
+
+    Surface(modifier = m.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.background_image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = resultado,
+                color = Color.Black,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(all = 10.dp)
+            )
+
+            Button(
+                onClick = {
+                    val indice = Random.nextInt(lista.size)
+                    resultado = lista[indice]
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Yellow,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text("Frase Diária")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BlessedPhrasesTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
--- gui/BlessedPhrases/app/src/main/java/dev/alanryan/blessedphrases/ui/theme/Theme.kt
+++ gui/BlessedPhrases/app/src/main/java/dev/alanryan/blessedphrases/ui/theme/Theme.kt
@@ -9,18 +9,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Yellow,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+
+    onPrimary = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Yellow,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+
+    onPrimary = Color.Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
