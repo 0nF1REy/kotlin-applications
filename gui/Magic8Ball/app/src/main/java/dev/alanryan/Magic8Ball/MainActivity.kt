017bd5b894bc4e6d30dd8cfbb54e2b62d20dbf68
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-02 19:14:25 -0300

    main: fix (Magic8Ball) - Improved main activity logic and stability
    - Refined the Android Magic8Ball app's main activity logic to improve stability and UI handling

--- gui/Magic8Ball/app/src/main/java/dev/alanryan/Magic8Ball/MainActivity.kt
+++ gui/Magic8Ball/app/src/main/java/dev/alanryan/Magic8Ball/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import dev.alanryan.Magic8Ball.ui.theme.Magic8BallTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Magic8BallTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Alan :)",
+                    App(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(modifier: Modifier = Modifier) {
+    val message = remember { mutableStateOf("") }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Está com dúvida?",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "Faça uma pergunta e irei te ajudar a decidir",
+            style = MaterialTheme.typography.bodyMedium,
+        )
+        Text(
+            text = message.value,
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Button(
+            onClick = {
+                val results = listOf("Com certeza!", "Não conte com isso!", "Talvez")
+                val index = Random.nextInt(results.size)
+                message.value = results[index]
+            },
+        ) {
+            Text("Perguntar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Magic8BallTheme {
-        Greeting("Android")
+        App()
     }
 }
